2fb532fd1c85b9ca3d9e40c53d467ccbbb05bed9
Ragib Shahriar <ragibshahriar2022@gmail.com>
2023-05-30 16:09:10 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import ExpenseItem from "./components/Expenses/ExpenseItem"
+import Expenses from "./components/Expenses/Expenses"
+import Card from "./components/UI/Card"
+import NewExpense from "./components/NewExpenses/NewExpense"
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ]
+  // const [count, setCount] = useState(0)
+
+  const addExpenseHandler = (expense) => {
+    console.log("IN APPP THINGTING")
+    console.log(expense)
+  }
+
+  return (
+    <div className="App">
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  )
+}
+
+export default App
